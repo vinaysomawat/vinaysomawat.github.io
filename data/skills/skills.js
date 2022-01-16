f7e804854608dc91dde1072337b90d146e87d879
vinaysomawat <vinaysomawat40@gmail.com>
2022-01-16 15:56:45 +0530

    populating skills through JSON

---
+++ data/skills/skills.js
@@ -0,0 +1,47 @@
+export default [
+	{
+		skillName: "JavaScript, C++",
+		color: "color-1",
+	    percentage: "width:70%"
+    },
+    {
+		skillName: "Angular, Reactjs",
+		color: "color-6",
+	    percentage: "width:50%"
+	},
+    {
+		skillName: "Nodejs, MongoDB",
+		color: "color-2",
+	    percentage: "width:40%"
+	},
+    {
+		skillName: "AWS(EC2, S3), Heroku, Netlify",
+		color: "color-3",
+	    percentage: "width:30%"
+	},
+    {
+		skillName: "HTML, Bootstrap, SCSS",
+		color: "color-4",
+	    percentage: "width:70%"
+	},
+    {
+		skillName: "GitHub, JIRA, Trello",
+		color: "color-7",
+	    percentage: "width:70%"
+	},
+    {
+		skillName: "Postman, Chrome DevTools",
+		color: "color-3",
+	    percentage: "width:80%"
+	},
+    {
+		skillName: "CleverTap, FreshDesk",
+		color: "color-5",
+	    percentage: "width:50%"
+	},
+    {
+		skillName: "VS Code",
+		color: "color-6",
+	    percentage: "width:70%"
+	}
+];
