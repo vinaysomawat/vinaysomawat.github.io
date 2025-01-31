f1053eff91f0573eed6df88618caf6c40f62fa46
Siddharth Krishna Senthilkumar <siddharthkrishna.ssk@gmail.com>
2025-01-30 20:32:10 -0600

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -1,5 +1,5 @@
 export const bio = [
-  "Hello, I'm Vinay Somawat!",
+  "Hello, I'm Sid Senthilkumar!",
   "<strong>Frontend Software Engineer</strong> with 4+ years of experience building scalable, user-focused web applications.",
   "Skilled in Angular, TypeScript, and performance optimization, I aim to leverage my expertise in delivering innovative solutions that drive measurable business outcomes.",
   "Passionate about mentoring, teamwork, and continuous improvement in engineering processes.",
